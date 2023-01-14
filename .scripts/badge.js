86eabde9ae95d7f2a09c6b15e9ee3c8f9203b531
Wes Reed <reedws@ucmail.uc.edu>
2023-01-14 12:14:14 -0500

    updated scripts

--- .scripts/badge.js
+++ .scripts/badge.js
@@ -1,34 +1,29 @@
-const git = require(`simple-git/promise`)(__dirname);
+const git = require(`simple-git`)(__dirname);
 const lineReplace = require(`line-replace`);
 
-async function main(){
+async function main() {
   const remote = await git.getRemotes(true);
-  let originRemote = remote.filter(r => r.name === `origin`)[0].refs.fetch
-  if(originRemote.startsWith(`git`)){
-    originRemote = `https://${originRemote.substring(4).slice(0,-4).replace(`:`,`/`)}`;
-  } else if (originRemote.startsWith(`https`)) {
-    originRemote = originRemote.slice(0, -4);
-  }
+  const originRemote = remote.filter(r => r.name === `origin`)[0].refs.fetch.substring(4).slice(0, -4).replace(`:`, `/`);
 
-  const result = `[![Assignment Checks](${originRemote}/actions/workflows/classroom.yml/badge.svg)](${originRemote}/actions/workflows/classroom.yml)`;
+  const result = `[![GitHub Classroom Workflow](https://${originRemote}/actions/workflows/classroom.yml/badge.svg)](https://${originRemote}/actions/workflows/classroom.yml)`;
 
   lineReplace({
     file: `README.md`,
     line: 3,
     text: result,
-    callback: ({file, line, text, replacedText, error}) => {
+    callback: ({ file, line, text, replacedText, error }) => {
       lineReplace({
         file: `README.md`,
         line: 8,
         text: `- [x] update the assignment checks above to the correct link. - Done Automatically`,
-        callback: ({file, line, text, replacedText, error}) => {
-          git.add([ `../README.md` ]);
+        callback: ({ file, line, text, replacedText, error }) => {
+          git.add([`../README.md`]);
           git.commit(`update README file`);
         }
       });
     }
   });
-  
+
 }
 
 main();
